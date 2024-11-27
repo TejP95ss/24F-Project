0ede5dd276383d5471550efb5d01c1d9b20bf218
TejP95ss <tejpatel9549s@gmail.com>
2024-11-27 13:10:16 -0500

    added a route for john's 1st user story where a review can be added to a specific coop position

--- api/backend/john_routes.py
+++ api/backend/john_routes.py
@@ -20,3 +20,23 @@ def get_position_details(id):
     response.status_code = 200
     return response
 
+# Route to add a new review for a specific co-op position(John's 1st story)
+@john.route('/reviews/<id>', methods=['POST'])
+def add_review(id):
+    review_data = request.json
+    rating = review_data['rating']
+    comments = review_data['review_text']
+    user_id = review_data['student_id']
+
+    query = f'''
+        INSERT INTO Reviews (rating, review_text, student_id, position_id)
+        VALUES ({rating}, '{comments}', {user_id}, {id})
+    '''
+    current_app.logger.info(f'Query: {query}')
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    response = make_response("Review added successfully")
+    response.status_code = 201
+    return response
