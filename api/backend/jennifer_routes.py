b186e25eab8148ec5bbcaa5c6d71ea13a7923ba8
athejaswi <aarushi.s.t1@gmail.com>
2024-11-28 13:04:13 -0500

    updating jennifer route 1

--- api/backend/jennifer_routes.py
+++ api/backend/jennifer_routes.py
@@ -5,11 +5,12 @@ jennifer = Blueprint('jennifer', __name__)
 
 # Gets summary data for a specific co-op
 @jennifer.route('/trends/<position_id>', methods=['GET'])
-def summary_data(position_id):
+def summary_data(id):
     query = f'''
-        SELECT r.review_id, p.position name, r.rating, r.review_text
-        FROM review r
-        JOIN position p ON r.position_id = p.id
+        SELECT c.name, cp.title, AVG(r.rating) AS avg_rating
+        FROM coop_position cp
+        JOIN company c ON cp.company_id = c.id
+        JOIN review r ON r.position_id = cp.id
         WHERE r.position_id = {id}
     '''
     cursor = db.get_db().cursor()
@@ -20,7 +21,7 @@ def summary_data(position_id):
     response.status_code = 200
     return response
 
-# Returns a specific co-op student (Natasha's 2nd story)
+# Gets recommended skills and majors for a co op
 @natasha.route('/user/<id>', methods=['GET'])
 def find_user(id):
     query = f'''
