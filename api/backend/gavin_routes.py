fd1f3724e56e094e935bb3b3b237d88645c72cab
alexkylander <kylander05@gmail.com>
2024-11-29 13:38:31 -0500

    added gavin route 1

--- api/backend/gavin_routes.py
+++ api/backend/gavin_routes.py
@@ -2,3 +2,22 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 from backend.db_connection import db
 
 gavin = Blueprint('gavin', __name__)
+
+# Gets student ids who are on linkedin and open to connect
+@gavin.route('/id/<id>', methods=['GET'])
+def find_searching_students(id):
+    query = f'''
+        SELECT s.id
+        FROM student s
+        WHERE s.linkedin IS NOT NULL
+        AND s.openToConnect = TRUE
+        WHERE s.id = {id}
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+
+    response = make_response(jsonify(theData))
+    response.status_code = 200
+    return response
