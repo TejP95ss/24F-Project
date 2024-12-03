3330c8fdf062695a31cd5d1ed2decc97b384b7e2
arushee4 <161550451+arushee4@users.noreply.github.com>
2024-12-03 16:55:51 -0500

    updating jennifer main page

--- app/src/pages/Jennifer_Home.py
+++ app/src/pages/Jennifer_Home.py
@@ -6,6 +6,7 @@ from modules.nav import SideBarLinks
 
 st.set_page_config(layout = 'wide')
 
+
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
@@ -13,13 +14,3 @@ st.title(f"Welcome {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
-
-# Navigation buttons for Jennifer's functionalities
-if st.button("View Co-op Trends and Summary", type="primary", use_container_width=True):
-    st.switch_page("Jennifer_Coop_Trends")
-
-if st.button("Explore Skills Insights", type="primary", use_container_width=True):
-    st.switch_page("Jennifer_Skills_Insights")
-
-if st.button("Manage Tasks", type="primary", use_container_width=True):
-    st.switch_page("Jennifer_Task_Management")
