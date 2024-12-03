9df9b17b5471847678c195e2193ba8649ec48405
alexkylander <kylander05@gmail.com>
2024-12-03 17:17:33 -0500

    implemented GCurrent_Users page

---
+++ app/src/pages/GCurrent_Users.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+SideBarLinks()
+
+# Title of the Page
+st.title("List of Current Student Users")
+
+# pulls the data
+results = requests.get(f"http://web-api:4000/current_users")
+
+# Display the details in a readable format
+st.subheader("Number of Current Student Users")
+st.dataframe(results)
