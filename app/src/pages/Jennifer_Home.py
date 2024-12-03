2d1a6aebf36025d4ed936605b05a9958a76f99d8
arushee4 <161550451+arushee4@users.noreply.github.com>
2024-12-03 14:33:01 -0500

    updating jennifer's landing page

--- app/src/pages/Jennifer_Home.py
+++ app/src/pages/Jennifer_Home.py
@@ -1,91 +1,15 @@
-##################################################
-# This is the main/entry-point file for the 
-# sample application for your project
-##################################################
-
-# Set up basic logging infrastructure
 import logging
-logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
-# streamlit supports reguarl and wide layout (how the controls
-# are organized/displayed on the screen).
 st.set_page_config(layout = 'wide')
 
-# If a user is at this page, we assume they are not 
-# authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false. 
-st.session_state['authenticated'] = False
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
 
-# Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel. 
-# IMPORTANT: ensure src/.streamlit/config.toml sets
-# showSidebarNavigation = false in the [client] section
-SideBarLinks(show_home=True)
-
-# ***************************************************
-#    The major content of this page
-# ***************************************************
-
-# set the title of the page and provide a simple prompt. 
-logger.info("Loading the Home page of the app")
-st.title('Rate My Co-Op')
-st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
-
-# For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user 
-# can click to MIMIC logging in as that mock user. 
-
-if st.button("Act as John, a First Time Coop Seeker", 
-            type = 'primary', 
-            use_container_width=True):
-    # when user clicks the button, they are now considered authenticated
-    st.session_state['authenticated'] = True
-    # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
-    # we add the first name of the user (so it can be displayed on 
-    # subsequent pages). 
-    st.session_state['first_name'] = 'John'
-    # finally, we ask streamlit to switch to another page, in this case, the 
-    # landing page for this particular user type
-    logger.info("Logging in as First Time Coop Seeker John")
-    st.switch_page('pages/John_Home.py')
-
-
-if st.button("Act as Gavin, a system administrator",
-             type = 'primary',
-             use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'sys_admin'
-    st.session_state['first_name'] = 'Gavin'
-    logger.info("Logging in as System Administrator Gavin")
-    st.switch_page('pages/Gavin_Home.py')
-
-if st.button('Act as Mohammad, an USAID worker', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
-
-if st.button('Act as System Administrator', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
-
-if st.button('Some Text On the Button',
-             type = 'secondary',
-             use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'Professor'
-    st.switch_page('pages/20_Admin_Home.py')
+st.title(f"Welcome {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
