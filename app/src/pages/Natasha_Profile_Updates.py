b8da4b7530ae6c3aa1d74e7a49fcd580b40b9ff8
N1har1kaB <niharikabanerjee@gmail.com>
2024-12-04 01:04:40 -0500

    Update Natasha_Profile_Updates.py

--- app/src/pages/Natasha_Profile_Updates.py
+++ app/src/pages/Natasha_Profile_Updates.py
@@ -1,29 +1,28 @@
 import streamlit as st
 import requests
 
-st.title("Add or Update Co-op Reviews")
-option = st.radio("What would you like to do?", ["Add Review", "Update Review"])
+st.title("Create and Update Profile")
+option = st.radio("What would you like to do?", ["Create Profile", "Update Profile"])
 
-# Add a Review
-if option == "Add Review":
-    st.header("Add a Review for a Position")
-    position_id = st.text_input("Coop Position ID: ")
-    student_id = st.text_input("Your Student ID: ")
-    rating = st.slider("Rating (1-5)", 1, 5, 3)
-    review_text = st.text_area("Write your review: ")
+# Create Profile
+if option == "Create Profile":
+    st.header("Create Profile")
+    username = st.text_input("Username: ")
+    profileType = st. # option to put either Seeker or Reviewer
+    openToConnect = st. # Option to put yes or no
 
-    if st.button("Submit Review"):
+    if st.button("Create Profile"):
         if position_id and student_id and review_text:
             data = {
-                "rating": rating,
-                "review_text": review_text,
-                "student_id": student_id
+                "username": username,
+                "profileType": profileType,
+                "openToConnect": openToConnect
             }
-            response = requests.post(f"http://web-api:4000/reviews/{position_id}", json=data)
+            response = requests.post(f"http://web-api:4000/students/{id}", json=data)
             if response.status_code == 201:
-                st.success("Review added successfully!")
+                st.success("Profile created successfully!")
             else:
-                st.error(f"Failed to add review. HTTP Status: {response.status_code}")
+                st.error(f"Failed to create profile. HTTP Status: {response.status_code}")
         else:
             st.warning("Please fill all fields!")
 
