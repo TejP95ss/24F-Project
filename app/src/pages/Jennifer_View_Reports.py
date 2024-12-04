92d09ad759213eb05353476f51a96af1c8860dd2
arushee4 <161550451+arushee4@users.noreply.github.com>
2024-12-04 11:21:06 -0500

    adding jennifer page 3

---
+++ app/src/pages/Jennifer_View_Reports.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+st.title("View Reports")
+
+if st.button("Fetch Reports Created by Data Analysts"):
+    try:
+        url = "http://web-api:4000/reports"
+        response = requests.get(url)
+        if response.status_code == 200:
+            reports = response.json()
+            if not reports:
+                st.info("No reports are available.")
+            else:
+                st.success("Reports:")
+                for report in reports:
+                    st.write(f"**Report Name**: {report['report_name']}")
+                    st.write(f"  **Industry Comparison**: {report['industry_compare']}")
+                    st.write(f"  **Created By**: {report['created_by']}")
+                    st.write(f"  **Timestamp**: {report['timestamp']}")
+                    st.write("---")
+        elif response.status_code == 404:
+            st.warning("No reports found.")
+        else:
+            st.error(f"Failed to fetch data. HTTP Status Code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"An error occurred: {str(e)}")
