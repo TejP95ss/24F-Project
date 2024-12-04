4cc8a65dc7cf5712bf32fdda4c577933da5c6813
TejP95ss <tejpatel9549s@gmail.com>
2024-12-04 09:46:17 -0500

    removed adding and updating review routes for john as the routes better fit other personas

--- api/backend/john/john_routes.py
+++ api/backend/john/john_routes.py
@@ -26,39 +26,6 @@ def get_position_details(id):
     response.status_code = 200
     return response
 
-# Route to add a new review for a specific co-op position(John's 1st story)
-@john.route('/reviews/<id>', methods=['POST'])
-def add_review(id):
-    review_data = request.json
-    rating = review_data['rating']
-    comments = review_data['review_text']
-    user_id = review_data['student_id']
-
-    position_check_query = f"SELECT id FROM coop_position WHERE id = {id}"
-    cursor = db.get_db().cursor()
-    cursor.execute(position_check_query)
-    position_exists = cursor.fetchone()
-    
-    if not position_exists:
-        current_app.logger.error(f"No position found with ID: {id}")
-        response = make_response(jsonify({"error": f"No position found with ID: {id}"}))
-        response.status_code = 404
-        return response
-
-    query = f'''
-        INSERT INTO review (rating, review_text, student_id, position_id)
-        VALUES ({rating}, '{comments}', {user_id}, {id})
-    '''
-    current_app.logger.info(f'Query: {query}')
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-
-    response = make_response("Review added successfully")
-    response.status_code = 201
-    return response
-
-
 # Route to add a new skill to a user's profile(John's 5th Story)
 @john.route('/user/<id>/skills', methods=['POST'])
 def add_user_skill(id):
@@ -121,38 +88,5 @@ def delete_user_skill(id):
     db.get_db().commit()
 
     response = make_response("Skill removed from user's profile successfully")
-    response.status_code = 200
-    return response
-
-# Route to update an existing review
-@john.route('/reviews/<id>', methods=['PUT'])
-def update_review(id):
-    review_data = request.json
-    rating = review_data['rating']
-    comments = review_data['review_text']
-
-    review_check_query = f"SELECT id FROM review WHERE id = {id}"
-    cursor = db.get_db().cursor()
-    cursor.execute(review_check_query)
-    review_exists = cursor.fetchone()
-    
-    if not review_exists:
-        current_app.logger.error(f"No review found with ID: {id}")
-        response = make_response(jsonify({"error": f"No review found with ID: {id}"}))
-        response.status_code = 404
-        return response
-
-    query = f'''
-        UPDATE review
-        SET rating = {rating}, review_text = '{comments}'
-        WHERE id = {id}
-    '''
-
-    current_app.logger.info(f'Query: {query}')
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-
-    response = make_response("Review successfully updated")
     response.status_code = 200
     return response
