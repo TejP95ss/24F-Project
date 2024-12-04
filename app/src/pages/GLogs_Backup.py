ab63c5dc72d7e3efc193a150a8ea641dedf4e819
alexkylander <kylander05@gmail.com>
2024-12-03 20:52:33 -0500

    updated GLogs_Backup page to reflect changes in the original route

--- app/src/pages/GLogs_Backup.py
+++ app/src/pages/GLogs_Backup.py
@@ -8,11 +8,14 @@ SideBarLinks()
 # Title of the Page
 st.title("Restore Backup Log")
 
+# Input box for version 
+id = st.text_input("Input Application ID to restore that application:")
+
 # Button to restore backup
 if st.button('Restore Backup',
              type = 'primary',
              use_container_width=True):
-    response = requests.put(f"http://web-api:4000/logs_backup")
+    response = requests.put(f"http://web-api:4000//logs_backup/{id}")
     if response.status_code == 200:
         st.success("Backup Restored Successfully!")
     else:
