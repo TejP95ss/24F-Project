8a6e4fd7dd11cb7455d8efd06ab4c9b4a6f808de
alexkylander <kylander05@gmail.com>
2024-12-04 13:13:33 -0500

    added implementation for deleting a review

--- app/src/pages/GDatabases.py
+++ app/src/pages/GDatabases.py
@@ -6,17 +6,38 @@ st.set_page_config(layout = 'wide')
 SideBarLinks()
 
 # Title of the Page
-st.title("Restore Backup Log")
+st.title("Manage Data")
+
+
+# Backup Log Management
+st.subheader("Restore Backup Log")
 
 # Input box for version 
-id = st.text_input("Input Application ID to restore that application:")
+app_id = st.text_input("Input Application ID to restore that application:")
 
 # Button to restore backup
 if st.button('Restore Backup',
              type = 'primary',
              use_container_width=True):
-    response = requests.put(f"http://web-api:4000//logs_backup/{id}")
+    response = requests.put(f"http://web-api:4000//logs_backup/{app_id}")
     if response.status_code == 200:
         st.success("Backup Restored Successfully!")
     else:
         st.error(f"Failed to restore backup. HTTP Status: {response.status_code}")
+
+
+# Removal of a review
+st.subheader("Remove Review")
+
+# Input box for review id
+id = st.text_input("Input Review ID to remove it")
+
+# Button to remove review
+if st.button('Delete Review',
+             type = 'primary',
+             use_container_width=True):
+    response = requests.delete(f"http://web-api:4000/review/{id}/remove")
+    if response.status_code == 200:
+        st.success("Review Deleted Successfully")
+    else:
+        st.error(f"Failed to delete review. HTTP Status: {response.status_code}")
