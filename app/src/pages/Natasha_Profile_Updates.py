2c306a2011e30a006791bf540050fe76cbdf89d5
N1har1kaB <niharikabanerjee@gmail.com>
2024-12-04 01:08:08 -0500

    Update OpenToConnect Preferences

--- app/src/pages/Natasha_Profile_Updates.py
+++ app/src/pages/Natasha_Profile_Updates.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 st.title("Create and Update Profile")
-option = st.radio("What would you like to do?", ["Create Profile", "Update Profile"])
+option = st.radio("What would you like to do?", ["Create Profile", "Change Connect Preferences"])
 
 # Create Profile
 if option == "Create Profile":
@@ -26,23 +26,20 @@ if option == "Create Profile":
         else:
             st.warning("Please fill all fields!")
 
-# Update a Review
-elif option == "Update Review":
-    st.header("Update an Existing Review")
-    review_id = st.text_input("Review ID")
-    rating = st.slider("New Rating (1-5)", 1, 5, 3)
-    review_text = st.text_area("Update your review")
+# Update Profile
+elif option == "Change Connect Preferences":
+    st.header("Change Your Connection Preferences")
+    openToConnect = st. # Option to put yes or no
 
-    if st.button("Update Review"):
-        if review_id and review_text:
+    if st.button("Update Profile"):
+        if openToConnect:
             data = {
-                "rating": rating,
-                "review_text": review_text
+                "openToConnect": openToConnect
             }
-            response = requests.put(f"http://web-api:4000/reviews/{review_id}", json=data)
+            response = requests.put(f"http://web-api:4000/user/{id}", json=data)
             if response.status_code == 200:
-                st.success("Review updated successfully!")
+                st.success("Preferences updated successfully!")
             else:
-                st.error(f"Failed to update review. HTTP Status: {response.status_code}")
+                st.error(f"Failed to update preferences. HTTP Status: {response.status_code}")
         else:
             st.warning("Please fill all fields!")
